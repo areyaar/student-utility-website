9d27dddd6d8d43d6c8f4845c3e73f8cb73e6c8ee
areyaar <saket.tanki@gmail.com>
2022-04-07 12:04:37 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app =  express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req,res)=>{
+    res.render('index.ejs');
+})
+app.get('/notes', (req, res)=>{
+    res.send("NoteTaking app in progress!");
+})
+app.get('/notes/:id', (req, res)=>{
+    const {id} = req.params;
+
+    res.send(`note number ${id}`);
+})
+app.get('/todo', (req,res)=>{
+    res.send("ToDo app!")
+})
+app.get('/search', (req,res)=>{
+    console.log(req.query);
+    res.send("Hi!");
+})
+//Catch all
+app.get('*', (req,res)=>{
+    res.send("Webpage does not exist!")
+})
+
+app.listen(3000, ()=>{
+    console.log("On 3000!");
+})
