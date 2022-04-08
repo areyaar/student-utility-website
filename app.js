638fc21dfe5b0f8e82a22201f1ce0702a84c6dd5
areyaar <saket.tanki@gmail.com>
2022-04-08 08:45:41 +0530

    added the landing page

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
 const express = require('express');
 const app =  express();
-
 const mongoose = require('mongoose');
+const Note = require('./models/notes');
+
 
 mongoose.connect('mongodb://localhost:27017/notes', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
-        console.log("MONGO CONNECTION OPEN!!!")
+        console.log("MONGO CONNECTION OPEN")
     })
     .catch(err => {
-        console.log("OH NO MONGO CONNECTION ERROR!!!!")
+        console.log("MONGO CONNECTION ERROR")
         console.log(err)
     })
 
 
 
+
+app.use(express.static('public'));
+
 app.set('view engine', 'ejs');
 
 app.get('/', (req,res)=>{
-    res.render('index.ejs');
+    res.render('landing.ejs');
 })
 app.get('/notes', (req, res)=>{
     res.render('notes.ejs');
