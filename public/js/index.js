fb71876bb100e9f7a7269e9d744b8fbe3a6aa544
areyaar <saket.tanki@gmail.com>
2022-04-14 19:09:26 +0530

    sab changa

--- public/js/index.js
+++ public/js/index.js
@@ -4,6 +4,8 @@ const leftParent = document.querySelector(".left")
 const signupDiv = document.querySelector(".signup-div")
 const close = document.querySelector(".close")
 
+
+
 signup.addEventListener('click' , ()=>{
     leftContent.classList.add('hide-left')
     leftParent.style.flex = "0 0 100%"
@@ -16,3 +18,33 @@ close.addEventListener('click',()=>{
     leftParent.style.flex = "0 0 30%"
     signupDiv.classList.add('signup-div')
 })
+
+
+let passwordInput = document.getElementById('txtPassword'),
+    toggle = document.getElementById('btnToggle'),
+    icon =  document.getElementById('eyeIcon');
+
+function togglePassword() {
+  if (passwordInput.type === 'password') {
+    passwordInput.type = 'text';
+    icon.classList.add("fa-eye-slash");
+    //toggle.innerHTML = 'hide';
+  } else {
+    passwordInput.type = 'password';
+    icon.classList.remove("fa-eye-slash");
+    //toggle.innerHTML = 'show';
+  }
+}
+
+function checkInput() {
+  //if (passwordInput.value === '') {
+  //toggle.style.display = 'none';
+  //toggle.innerHTML = 'show';
+  //  passwordInput.type = 'password';
+  //} else {
+  //  toggle.style.display = 'block';
+  //}
+}
+
+toggle.addEventListener('click', togglePassword, false);
+passwordInput.addEventListener('keyup', checkInput, false);
